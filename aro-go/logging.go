0acaa4cf764f200f4795e8bc7f92c34d2ea41ffe
Shivkumar Ople <shivkumarople@gmail.com>
2022-04-27 01:08:33 +0530

    Logging stuff with Go

---
+++ aro-go/logging.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main(){
+	fmt.Println("Hello, world!")
+
+	f, err := os.OpenFile("golang-demo.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	log.Print("This is our first log message in Go!")
+
+
+
+}
